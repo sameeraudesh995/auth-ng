b092ca679ffa847296e4fe3870f6d362cf54be00
sameeraudesh995 <sameeraudesh.2225@gmail.com>
2025-02-16 22:30:41 +0530

    initialize

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {WelcomePageComponent} from './components/welcome-page/welcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: WelcomePageComponent,
+  }
+];
